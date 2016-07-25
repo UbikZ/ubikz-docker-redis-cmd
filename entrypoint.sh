0794cf541a31589dbddee743e33e515f6ac0b1bc
Gabriel Malet <gabrielmalet@gmail.com>
2016-07-25 11:50:27 +0200

    Clean entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-echo ${REDIS_CMD_USER:-root}
-echo ${REDIS_CMD_PASS:-toor}
-
 CMD="redis-commander --redis-host redis -p 8080 --http-u $REDIS_CMD_USER --http-p $REDIS_CMD_PASS"
 exec $CMD
